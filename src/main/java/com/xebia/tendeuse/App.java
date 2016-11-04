6603d3c8809219f4b09be4c6453ada283b2eb0b5
ZM2B9DAN <ziad-externe.mesalmi@edf.fr>
2016-11-04 14:06:59 +0100

    Initialisation du projet

---
+++ src/main/java/com/xebia/tendeuse/App.java
@@ -0,0 +1,13 @@
+package com.xebia.tendeuse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/xebia/tendeuse/bean/Direction.java
@@ -0,0 +1,5 @@
+package com.xebia.tendeuse.bean;
+
+public enum Direction {
+
+}
